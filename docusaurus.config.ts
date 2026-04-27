ebae75506a5e6b7ed17f5214ace47f12a0dd2188
Nurul Ain <ain@abatacommerce.com>
2026-04-27 17:13:44 +0800

    base url change for staging

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,12 @@ const config: Config = {
   url: 'https://your-docusaurus-site.example.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/mykkp-pemohon-docs',
+
+  // for Github, set to this
+  // baseUrl: '/mykkp-pemohon-docs',
+
+  // for Cloudfare, set to this
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
