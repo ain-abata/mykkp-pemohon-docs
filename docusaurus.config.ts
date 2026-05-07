1b8425d867bb3309d5138e490edd9b8d05283c5e
Atika <Nuratika@abatacommerce.com>
2026-05-07 16:57:55 +0800

    update

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -27,7 +27,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://fcbcc238.mykkp-pemohon-docs-stg.pages.dev/',
+  url: 'https://faq-tk-ok.mykkp-pemohon-docs-stg.pages.dev/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
