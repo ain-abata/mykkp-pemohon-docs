2b726ae438ef41fd31192d018e5f367f942935a5
Atika <Nuratika@abatacommerce.com>
2026-05-13 11:03:27 +0800

    add algolia Ask AI integration

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -106,9 +106,10 @@ const config: Config = {
     apiKey: '877953df5cf2bdd57f792442f023df7b',
     indexName: 'MyKKP DOC',
     contextualSearch: false,
-
+    
     askAi: {
     assistantId: 'f6f425ac-7c90-4fe7-823b-2a0865f57222',
+    indexName: 'Documentation assistant',
     },
 
   },
