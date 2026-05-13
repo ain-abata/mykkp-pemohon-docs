226afdc5cd97aafac7357727ff82af71b43efb38
Atika <Nuratika@abatacommerce.com>
2026-05-13 08:53:46 +0800

    update contextualSerach to false

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -105,7 +105,7 @@ const config: Config = {
     appId: 'P14KWFGPWB',
     apiKey: '877953df5cf2bdd57f792442f023df7b',
     indexName: 'MyKKP DOC',
-    contextualSearch: true,
+    contextualSearch: false,
 
     askAi: {
     assistantId: 'f6f425ac-7c90-4fe7-823b-2a0865f57222',
